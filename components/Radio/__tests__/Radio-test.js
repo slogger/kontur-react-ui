dae0c7ea0284afd9a751df59e1dbc9f4410b8bf3
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2016-05-26 08:24:03 +0500

    added some tests on Radio

---
+++ components/Radio/__tests__/Radio-test.js
@@ -0,0 +1,45 @@
+import Radio from '../Radio';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('Radio', () => {
+  it('properly calls onChange prop', () => {
+    let checked = false;
+    const onChange = (e) => {checked = e.target.checked;};
+    const wrapper = shallow(<Radio onChange={onChange}/>);
+    wrapper.find('input').simulate('change', { target: { checked: true } });
+    expect(checked).toBe(true);
+  });
+
+  /* // Not working - needs component to be mounted
+  it('do not calls onChange prop if disabled', () => {
+    let checked = false;
+    const onChange = (e) => {checked = e.target.checked;};
+    const wrapper = shallow(<Radio disabled onChange={onChange}/>);
+    wrapper.find('input').simulate('change', { target: { checked: true } });
+    expect(checked).toBe(false);
+  });
+  */
+
+  it('contains children in output', () => {
+    const child = 'SomeTextHere'
+    const wrapper = shallow(<Radio>{child}</Radio>);
+    expect(wrapper.contains(child)).toBe(true);
+  })
+
+  it('properly handles checked prop', () => {
+    const wrapper = shallow(<Radio checked />);
+    expect(wrapper.find('input').node.props.checked).toBeTruthy();
+
+    wrapper.setProps({checked: false})
+    expect(wrapper.find('input').node.props.checked).toBeFalsy();
+  })
+
+  it('properly handles disabled prop', () => {
+    const wrapper = shallow(<Radio disabled />);
+    expect(wrapper.find('input').node.props.disabled).toBeTruthy();
+
+    wrapper.setProps({disabled: false})
+    expect(wrapper.find('input').node.props.disabled).toBeFalsy();
+  })
+});
